5dbd1f46df20e2329bb8cb715f8abd604f451c1b
Aravind Ramesh <aramesh@hach.com>
2015-12-21 19:25:17 +0530

    custom fixes 1) position container fix 2) width fix 3) animation fix

--- dist/angular-notify.js
+++ dist/angular-notify.js
@@ -1,5 +1,5 @@
-angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','$templateCache','$rootScope',
-    function($timeout,$http,$compile,$templateCache,$rootScope){
+angular.module('cgNotify', []).factory('notify', ['$timeout','$http','$compile','$templateCache','$rootScope',
+    function ($timeout,$http,$compile,$templateCache,$rootScope) {
 
         var startTop = 10;
         var verticalSpacing = 15;
@@ -8,25 +8,30 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
         var position = 'center';
         var container = document.body;
         var maximumOpen = 0;
+        var parentalPosition = false;
 
         var messageElements = [];
         var openNotificationsScope = [];
 
-        var notify = function(args){
+        var notify = function (args) {
 
-            if (typeof args !== 'object'){
-                args = {message:args};
+            if (typeof args !== 'object') {
+                args = {message: args};
             }
 
-            args.duration = args.duration ? args.duration : defaultDuration;
+            args.duration = !angular.isUndefined(args.duration) ? args.duration : defaultDuration;
             args.templateUrl = args.templateUrl ? args.templateUrl : defaultTemplateUrl;
             args.container = args.container ? args.container : container;
             args.classes = args.classes ? args.classes : '';
+            args.parentalPosition = args.parentalPosition ? args.parentalPosition : parentalPosition;
 
             var scope = args.scope ? args.scope.$new() : $rootScope.$new();
+            scope.$slide = false;
             scope.$position = args.position ? args.position : position;
             scope.$message = args.message;
             scope.$classes = args.classes;
+            scope.$parentalPosition = args.parentalPosition;
+
             scope.$messageTemplate = args.messageTemplate;
 
             if (maximumOpen > 0) {
@@ -36,94 +41,98 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
                 }
             }
 
-            $http.get(args.templateUrl,{cache: $templateCache}).success(function(template){
+            $http.get(args.templateUrl, {cache: $templateCache}).success(function (template) {
 
                 var templateElement = $compile(template)(scope);
-                templateElement.bind('webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd', function(e){
-                    if (e.propertyName === 'opacity' || e.currentTarget.style.opacity === 0 || 
-                        (e.originalEvent && e.originalEvent.propertyName === 'opacity')){
-
+                templateElement.bind('webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd', function (e) {
+                    if(e.propertyName === 'opacity' || e.currentTarget.style.opacity === 0 ||
+                      (e.originalEvent && e.originalEvent.propertyName === 'opacity')) {
                         templateElement.remove();
-                        messageElements.splice(messageElements.indexOf(templateElement),1);
-                        openNotificationsScope.splice(openNotificationsScope.indexOf(scope),1);
+                        messageElements.splice(messageElements.indexOf(templateElement), 1);
+                        openNotificationsScope.splice(openNotificationsScope.indexOf(scope), 1);
                         layoutMessages();
                     }
                 });
 
-                if (args.messageTemplate){
+                if (args.messageTemplate) {
                     var messageTemplateElement;
-                    for (var i = 0; i < templateElement.children().length; i ++){
-                        if (angular.element(templateElement.children()[i]).hasClass('cg-notify-message-template')){
+                    for (var i = 0; i < templateElement.children().length; i++) {
+                        if (angular.element(templateElement.children()[i]).hasClass('fm-notify-message-template')) {
                             messageTemplateElement = angular.element(templateElement.children()[i]);
                             break;
                         }
                     }
-                    if (messageTemplateElement){
+                    if (messageTemplateElement) {
                         messageTemplateElement.append($compile(args.messageTemplate)(scope));
                     } else {
-                        throw new Error('cgNotify could not find the .cg-notify-message-template element in '+args.templateUrl+'.');
+                        throw new Error('cgNotify could not find the .fm-notify-message-template element in ' + args.templateUrl + '.');
                     }
+                }
+
+                if (args.parentalPosition === true) {
+                    angular.element(document.body).find(args.container).addClass('fm-notify-notification-parent');
                 }
 
                 angular.element(args.container).append(templateElement);
                 messageElements.push(templateElement);
 
-                if (scope.$position === 'center'){
-                    $timeout(function(){
-                        scope.$centerMargin = '-' + (templateElement[0].offsetWidth /2) + 'px';
+                if (scope.$position === 'center') {
+                    $timeout(function () {
+                        scope.$centerMargin = '-' + (templateElement[0].offsetWidth / 2) + 'px';
                     });
                 }
 
-                scope.$close = function(){
-                    templateElement.css('opacity',0).attr('data-closing','true');
+                scope.$close = function () {
+                    scope.$slide = true;
+                    templateElement.css('opacity', 0).attr('data-closing', 'true');
                     layoutMessages();
                 };
 
-                var layoutMessages = function(){
+                var layoutMessages = function () {
                     var j = 0;
                     var currentY = startTop;
-                    for(var i = messageElements.length - 1; i >= 0; i --){
+                    for (var i = messageElements.length - 1; i >= 0; i--) {
                         var shadowHeight = 10;
                         var element = messageElements[i];
                         var height = element[0].offsetHeight;
                         var top = currentY + height + shadowHeight;
-                        if (element.attr('data-closing')){
-                            top += 20;
-                        } else {
+                        if (element.attr('data-closing')) {
+                            //top -= (height + shadowHeight);
+                        }else {
                             currentY += height + verticalSpacing;
+                            element.css('top', top + 'px').css('margin-top', '-' + (height + shadowHeight) + 'px').css('visibility', 'visible');
                         }
-                        element.css('top',top + 'px').css('margin-top','-' + (height+shadowHeight) + 'px').css('visibility','visible');
-                        j ++;
+                        j++;
                     }
                 };
 
-                $timeout(function(){
+                $timeout(function () {
                     layoutMessages();
                 });
 
-                if (args.duration > 0){
-                    $timeout(function(){
+                if (args.duration > 0) {
+                    $timeout(function () {
                         scope.$close();
-                    },args.duration);
+                    }, args.duration);
                 }
 
-            }).error(function(data){
-                    throw new Error('Template specified for cgNotify ('+args.templateUrl+') could not be loaded. ' + data);
+            }).error(function (data) {
+                throw new Error('Template specified for cgNotify (' + args.templateUrl + ') could not be loaded. ' + data);
             });
 
             var retVal = {};
-            
-            retVal.close = function(){
-                if (scope.$close){
+
+            retVal.close = function () {
+                if (scope.$close) {
                     scope.$close();
                 }
             };
 
-            Object.defineProperty(retVal,'message',{
-                get: function(){
+            Object.defineProperty(retVal, 'message', {
+                get: function () {
                     return scope.$message;
                 },
-                set: function(val){
+                set: function (val) {
                     scope.$message = val;
                 }
             });
@@ -134,20 +143,21 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
 
         };
 
-        notify.config = function(args){
+        notify.config = function (args) {
             startTop = !angular.isUndefined(args.startTop) ? args.startTop : startTop;
             verticalSpacing = !angular.isUndefined(args.verticalSpacing) ? args.verticalSpacing : verticalSpacing;
             defaultDuration = !angular.isUndefined(args.duration) ? args.duration : defaultDuration;
             defaultTemplateUrl = args.templateUrl ? args.templateUrl : defaultTemplateUrl;
             position = !angular.isUndefined(args.position) ? args.position : position;
             container = args.container ? args.container : container;
+            parentalPosition = args.parentalPosition ? args.parentalPosition : parentalPosition;
             maximumOpen = args.maximumOpen ? args.maximumOpen : maximumOpen;
         };
 
-        notify.closeAll = function(){
-            for(var i = messageElements.length - 1; i >= 0; i --){
+        notify.closeAll = function () {
+            for (var i = messageElements.length - 1; i >= 0; i--) {
                 var element = messageElements[i];
-                element.css('opacity',0);
+                element.css('opacity', 0);
             }
         };
 
@@ -155,30 +165,49 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
     }
 ]);
 
-angular.module('cgNotify').run(['$templateCache', function($templateCache) {
-  'use strict';
+angular.module('cgNotify').run(['$templateCache', function ($templateCache) {
+    'use strict';
 
-  $templateCache.put('angular-notify.html',
-    "<div class=\"cg-notify-message\" ng-class=\"[$classes, \n" +
-    "    $position === 'center' ? 'cg-notify-message-center' : '',\n" +
-    "    $position === 'left' ? 'cg-notify-message-left' : '',\n" +
-    "    $position === 'right' ? 'cg-notify-message-right' : '']\"\n" +
-    "    ng-style=\"{'margin-left': $centerMargin}\">\n" +
-    "\n" +
-    "    <div ng-show=\"!$messageTemplate\">\n" +
-    "        {{$message}}\n" +
-    "    </div>\n" +
-    "\n" +
-    "    <div ng-show=\"$messageTemplate\" class=\"cg-notify-message-template\">\n" +
-    "        \n" +
-    "    </div>\n" +
-    "\n" +
-    "    <button type=\"button\" class=\"cg-notify-close\" ng-click=\"$close()\">\n" +
-    "        <span aria-hidden=\"true\">&times;</span>\n" +
-    "        <span class=\"cg-notify-sr-only\">Close</span>\n" +
-    "    </button>\n" +
-    "\n" +
-    "</div>"
-  );
+    $templateCache.put('angular-notify.html',
+      "<div class=\"cg-notify-message\" ng-class=\"[$classes, \r" +
+      "\n" +
+      "    $position === 'center' ? 'cg-notify-message-center' : '',\r" +
+      "\n" +
+      "    $position === 'left' ? 'cg-notify-message-left' : '',\r" +
+      "\n" +
+      "    $position === 'right' ? 'cg-notify-message-right' : '']\"\r" +
+      "\n" +
+      "    ng-style=\"{'margin-left': $centerMargin}\">\r" +
+      "\n" +
+      "\r" +
+      "\n" +
+      "    <div ng-show=\"!$messageTemplate\">\r" +
+      "\n" +
+      "        {{$message}}\r" +
+      "\n" +
+      "    </div>\r" +
+      "\n" +
+      "\r" +
+      "\n" +
+      "    <div ng-show=\"$messageTemplate\" class=\"cg-notify-message-template\">\r" +
+      "\n" +
+      "        \r" +
+      "\n" +
+      "    </div>\r" +
+      "\n" +
+      "\r" +
+      "\n" +
+      "    <button type=\"button\" class=\"cg-notify-close\" ng-click=\"$close()\">\r" +
+      "\n" +
+      "        <span aria-hidden=\"true\">&times;</span>\r" +
+      "\n" +
+      "        <span class=\"cg-notify-sr-only\">Close</span>\r" +
+      "\n" +
+      "    </button>\r" +
+      "\n" +
+      "\r" +
+      "\n" +
+      "</div>"
+    );
 
 }]);
